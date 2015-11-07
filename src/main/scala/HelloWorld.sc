fb85b0b009cdc43eda7263839534071db25fa6a4
zhouzhihu <zhouzhihubeyond@sina.com>
2015-11-07 12:13:30 +0800

    Initial commit

---
+++ src/main/scala/HelloWorld.sc
@@ -0,0 +1,4 @@
+val set3=Set(1,2,3)
+val set4=scala.collection.mutable.Set(2,3)
+set4+=1
+set4
---
+++ src/main/scala/SetAppDemo.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/10/29.
+ */
+object SetAppDemo extends App{
+  println("run from jar")
+
+
+
+  println("test")
+}
